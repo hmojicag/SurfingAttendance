918f43b6f526b5e95012a0e5e135b44839bc80cb
Mojica García Hazael Fernando <MojicaGarciaHazaelF@JohnDeere.com>
2023-05-09 21:51:38 -0500

    Adding support for deleting a user

--- app/src/main/java/mx/ssaj/surfingattendance/ui/users/upsert/UserUpsertViewModel.java
+++ app/src/main/java/mx/ssaj/surfingattendance/ui/users/upsert/UserUpsertViewModel.java
@@ -3,15 +3,18 @@ package mx.ssaj.surfingattendance.ui.users.upsert;
 import android.app.Application;
 import androidx.lifecycle.AndroidViewModel;
 import mx.ssaj.surfingattendance.data.model.Users;
+import mx.ssaj.surfingattendance.data.repositories.BioPhotosRepository;
 import mx.ssaj.surfingattendance.data.repositories.UsersRepository;
 
 public class UserUpsertViewModel extends AndroidViewModel {
 
     private UsersRepository usersRepository;
+    private BioPhotosRepository bioPhotosRepository;
 
     public UserUpsertViewModel(Application application) {
         super(application);
         usersRepository = new UsersRepository(application);
+        bioPhotosRepository = new BioPhotosRepository(application);
     }
 
     public void insert(Users user) {
@@ -20,6 +23,11 @@ public class UserUpsertViewModel extends AndroidViewModel {
 
     public void update(Users user) {
         usersRepository.update(user);
+    }
+
+    public void delete(int userId) {
+        bioPhotosRepository.deleteForUser(userId);
+        usersRepository.deleteById(userId);
     }
 
     public Users findById(int userId) {
